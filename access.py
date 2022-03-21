c3f3415e344f30b5e2547d1367dccf197c1f31a2
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-03-21 23:06:20 +0530

    Update access.py

--- access.py
+++ access.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 '''
-Author: PriyanshBordia, 
+Author: PriyanshBordia
 '''
 
-#Change this
-rollNo = '19ucs257'
-pwd = 'lnm$456a'
+# Credentials
+rollNo = '[rollNo]'
+pwd = '[password]'
 
 from time import sleep
 from selenium import webdriver
@@ -24,13 +24,10 @@ driver.get("https://172.22.2.6/connect/PortalMain")
 try:
 	username_box = driver.find_element(By.ID, 'LoginUserPassword_auth_username')
 	username_box.send_keys(rollNo)
-
 	pwd_box = driver.find_element(By.ID, 'LoginUserPassword_auth_password')
 	pwd_box.send_keys(pwd)
-
 	login_button = driver.find_element(By.ID, 'UserCheck_Login_Button')
 	login_button.click()
-
 	print("Successfully authenticated.!!")
 except Exception as e:
 	raise e

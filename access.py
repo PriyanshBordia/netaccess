82c126c6908981c091016a22efe1f28127e4bb83
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-03-21 15:37:03 +0530

    Update access.py

--- access.py
+++ access.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
 '''
-Author: jeeva2812, 
+Author: PriyanshBordia, 
 '''
 
 #Change this
-rollNo = '[username]'
-pwd = '[password]'
+rollNo = '19ucs257'
+pwd = 'lnm$456a'
 
 import selenium
 import selenium.webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+webdriver = "/Users/priyansh/Desktop/GitHub/netaccess/venv/lib/python3.9/site-packages/selenium/webdriver/chrome/webdriver.py"
+options = "/Users/priyansh/Desktop/GitHub/netaccess/venv/lib/python3.9/site-packages/selenium/webdriver/chrome/options.py"
+service = "/Users/priyansh/Desktop/GitHub/netaccess/venv/lib/python3.9/site-packages/selenium/webdriver/chrome/service.py"
 
+import time
+from selenium.webdriver.chrome.service import Service
+service = Service(options)
+service.start()
+driver = webdriver.Remote(service.service_url)
+driver.get('http://www.google.com/');
+time.sleep(5) # Let the user actually see something!
+driver.quit()
+
+"""
 chrome_options = Options()
-chrome_options.add_argument("--headless") #Remove this to see the process
+# chrome_options.add_argument("--headless") #Remove this to see the process
 driver = selenium.webdriver.Chrome(options=chrome_options)
 
 driver.get('http://172.2.2.6/connect')
@@ -38,3 +51,4 @@ approve_button = driver.find_element(By.ID, 'approveBtn')
 approve_button.click()
 
 #print("Successfully authenticated")
+"""
